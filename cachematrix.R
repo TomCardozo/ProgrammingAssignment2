8c42d244a122d337d0875f5238e0c88c60368829
Tom Cardozo <tom.cardozo@gmail.com>
2015-09-26 23:38:40 -0400

    Start commenting; change some variable names for clarity

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## This pair of functions, makeCacheMatrix and cacheSolve, are used to create
+## and operate on a matrix representation to provide a defered calculation of
+## the matrix inverse and to cache that calculation to prevent wasteful recalculation.
+## 
+## makeCacheMatrix creates the 'representation' as a list of four function closures which
+## provide get and set access to the matrix and its inverse.
+##
+## cacheSolve takes the matrix representation as input and returns the inverse
+## of the matrix, either calculating the inverse or returning the cached value.
+
+
 
 ## Write a short comment describing this function
 
@@ -28,14 +38,16 @@ makeCacheMatrix <- function(the_matrix = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        the_inverse <- x$getinverse()
-        if (!is.null(the_inverse)) {
-        	message("getting cached data")
-        	return(the_inverse)
-        }
-        the_matrix <- x$get()
-        the_inverse <- solve(the_matrix)
-        x$setinverse(the_inverse)
-        the_inverse   
+	## Return a matrix that is the inverse of 'x'
+	stored_inverse <- x$getinverse()
+	if (!is.null(stored_inverse)) {
+		message("getting cached data")
+		return(stored_inverse)
+	}
+	stored_matrix <- x$get()
+	solved_inverse <- solve(stored_matrix)
+	x$setinverse(solved_inverse)
+	solved_inverse
 }
+
+
