a383c588aefc2a3aed6784b21e1056597f3ec1d1
Tom Cardozo <tom.cardozo@gmail.com>
2015-09-27 00:43:24 -0400

    Modify messages; add sanitycheck() test function.

--- cachematrix.R
+++ cachematrix.R
@@ -41,13 +41,28 @@ cacheSolve <- function(x, ...) {
 	## Return a matrix that is the inverse of 'x'
 	stored_inverse <- x$getinverse()
 	if (!is.null(stored_inverse)) {
-		message("getting cached data")
+		message("Getting cached inverse matrix.")
 		return(stored_inverse)
 	}
+	message("Returning calculated inverse matrix.")
 	stored_matrix <- x$get()
 	solved_inverse <- solve(stored_matrix)
 	x$setinverse(solved_inverse)
 	solved_inverse
 }
 
+## This function is a quick test to verify that things are not broken
+##
+sanitycheck <- function() {
+	mm <- matrix(c(1, 0, 0, 0, 1, 0, 0, 0, 7), 3, 3)
+	print("The test matrix:")
+	print(mm)
+	cmm <- makeCacheMatrix()
+	cmm$set(mm)
 
+	print("The calculated inverse of test matrix:")
+	print(cacheSolve(cmm))			# should be inverse matrix
+	
+	print("Expect: message, then the cached inverse of test matrix:")
+	print(cacheSolve(cmm))			# should be inverse matrix preceded by message
+}
